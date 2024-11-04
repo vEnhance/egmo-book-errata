6bcbaed6e178727e84d44e3314a38824f4d6979b
Evan Chen <evan@evanchen.cc>
2024-11-03 21:43:36 -0500

    chore: Move errata to textbooks/

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 latexmk
-gcloud storage cp main.pdf gs://web.evanchen.cc/upload/geombook-errata.pdf --cache-control='max-age=0, private'
+gcloud storage cp main.pdf gs://web.evanchen.cc/textbooks/geombook-errata.pdf --cache-control='max-age=0, private'
